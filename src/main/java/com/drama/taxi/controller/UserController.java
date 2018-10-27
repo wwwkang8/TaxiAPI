34e2b8c520515e607246e64edadc1ddbab1fe0fd
JeongHo <wwwkang8@gmail.com>
2018-10-27 21:58:52 +0900

    Usercontroller 잘못된 import 제거

--- src/main/java/com/drama/taxi/controller/UserController.java
+++ src/main/java/com/drama/taxi/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.drama.taxi.controller;
 
 import com.drama.taxi.domain.User;
-import com.drama.taxi.service.BookingService;
 import com.drama.taxi.service.UserService;
-import oracle.jdbc.proxy.annotation.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
