3e7b8e135627877568c9ae90b3cb4c29fa444bf2
JeongHo <wwwkang8@gmail.com>
2018-10-27 18:10:25 +0900

    특정 승객의 배차리스트 테스트 코드

--- src/main/java/com/drama/taxi/repository/BookingRepository.java
+++ src/main/java/com/drama/taxi/repository/BookingRepository.java
@@ -4,6 +4,7 @@ import com.drama.taxi.domain.Booking;
 import com.drama.taxi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,5 +15,8 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     Booking findBookingById(Long id);
 
+    //@Query("select b from Booking b, User u where b.user_id=u.id and b.user_id=:id")
+    @Query("select b from Booking b inner join b.user u where u.id = :id")
+    List<Booking> findPassengerBookingList(@Param("id")Long id);
 
 }
--- src/test/java/com/drama/taxi/TaxiApplicationTests.java
+++ src/test/java/com/drama/taxi/TaxiApplicationTests.java
@@ -1,7 +1,9 @@
 package com.drama.taxi;
 
+import com.drama.taxi.repository.BookingRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TaxiApplicationTests {
 
+
+    @Autowired
+    BookingRepository bookingRepository;
+
     @Test
     public void contextLoads() {
     }
+
+    @Test
+    public void bookingList(){
+        Long id=Long.parseLong("1");
+        System.out.println(bookingRepository.findPassengerBookingList(id));
+    }
+
+
 
 }
